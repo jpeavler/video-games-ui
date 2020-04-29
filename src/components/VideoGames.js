7c37e80be0420776a3c782b780c8b69d23044fd7
Joshua Peavler <joshuaapeavler@gmail.com>
2020-04-29 11:34:33 -0600

    Implemented UI get video games utilizing React hooks

--- src/components/VideoGames.js
+++ src/components/VideoGames.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
+import VideoGame from './VideoGame'
 
-const Home = () =>{
+const VideoGames = () =>{
+    const [videoGames, setVideoGames] = useState([]);
+    const getVideoGames = () =>{
+        fetch(`${process.env.REACT_APP_API_URL}/api/video-games`)
+            .then(response => response.json())
+            .then(videoGames => setVideoGames(videoGames))
+    }
+    const displayGames = videoGames.map((game) =>{
+        return <VideoGame key={game._id}
+                game={game}
+                />
+    });
+    useEffect(() =>{
+        getVideoGames();
+    })
     return (
-        <>
-            <h1>Home</h1>
-            <Link to='/' class="link">Home</Link>
-        </>
+    <>
+        <Link to='/' class="link">Home</Link>
+        <h1>Video Game Collection</h1>
+        {displayGames}
+    </>
     )
 }
 
-export default Home;
+export default VideoGames;

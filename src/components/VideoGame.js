368e2c0b8938d8bc5ab0950b11f16034d9aea04a
Joshua Peavler <joshuaapeavler@gmail.com>
2020-04-30 14:40:44 -0600

    Implemented all Video Game datapoints into view

--- src/components/VideoGame.js
+++ src/components/VideoGame.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 const VideoGame = ({game, deleteGame, updateGame}) =>{
+    let displayPlayers;
+    let max = parseInt(game.num_players_max);
+    if(max === 1){
+        displayPlayers = <p>Single Player</p>
+    }else if(game.num_players_min === game.num_players_max){
+        displayPlayers = <p>Players: {game.num_players_max}</p>
+    }else{
+        displayPlayers = <p>Players: {game.num_players_min}-{game.num_players_max}</p>
+    }
     return(
         <div>
             <h4>{game.title}</h4>
+            <p>{game.platform}</p>
+            {displayPlayers}
+            <p>{game.desc}</p>
             <button onClick={() => deleteGame(game._id)}>Delete</button>
             <button onClick={() => updateGame(game)}>Edit</button>
         </div>
